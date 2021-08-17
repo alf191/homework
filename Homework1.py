7fb6018321c162f90b080c3b44c2a81ea2d84816
alf191 <88853107+alf191@users.noreply.github.com>
2021-08-17 21:25:31 +0200

    Update Homework1.py

--- Homework1.py
+++ Homework1.py
@@ -12,3 +12,25 @@ if __name__ == "__main__":
     print(Perimetr(d))
     print(squaere(d))
 
+    
+    Zadanie2.
+    
+    
+    def find_entrance (f, n):
+
+    x = n // (f * 4)
+    e = x + 1
+    print(e)
+def find_floor(f, n):
+    z = n % (f * 4)
+    b = z // 4
+    fl = b + 1
+    print(fl)
+if __name__ == "__main__":
+    floors = int(input("Chislo etazei: "))
+    flat_noom = int(input("Nomer kwartiry: "))
+    entrance = find_entrance(floors, flat_noom)
+    floor = find_floor(floors, flat_noom)
+    print(entrance, floor)
+
+    print()
