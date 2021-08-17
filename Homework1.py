613465e02912344516896741ac88c6943f033ffa
alf191 <88853107+alf191@users.noreply.github.com>
2021-08-17 20:56:58 +0200

    Add files via upload

---
+++ Homework1.py
@@ -0,0 +1,14 @@
+import math
+
+
+def Perimetr(diametr):
+    print(math.pi * d)
+
+def squaere(diametr):
+    l = d / 2
+    print(math.pi * (l * l))
+if __name__ == "__main__":
+    d = int(input("Diametr skoworodki: "))
+    print(Perimetr(d))
+    print(squaere(d))
+
