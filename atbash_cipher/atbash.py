0dd5e628c75b3cde80f7d3214745d0cea470fb7e
alf191 <88853107+alf191@users.noreply.github.com>
2021-09-15 23:23:11 +0300

    Update atbash.py

--- atbash_cipher/atbash.py
+++ atbash_cipher/atbash.py
@@ -1,24 +1,15 @@
-#!/usr/bin/env python
+import string
 
-"""
-Напишите программу, которая кодирует и декодирует текст шифром Атбаш.
-В этом шифре каждая i-ая буква алфавита заменяется i-ой буквой с его конца, например, для латинского алфавита: a - z, b - y и т.д.
+def atbash_code(j: str):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    y = j.lower().replace(' ','').translate(str.maketrans(' ', ' ', string.punctuation + '—'))
 
-- заглавные буквы переводятся в строчные
-- пробельные символы и знаки препинания опускаются
-- шифр идёт блоками по 5 символов, между ними пробел
+    return y.translate(str.maketrans(alphabet + alphabet.upper(), alphabet[::-1] + alphabet.upper()[::-1]))
 
-Пример:
+def len_5(b: str):
 
-`Bambarbia, Kirgudu` -> `yznyz iyrzp ritfw f`
-"""
-
-BLOCK_SIZE = 5
+    for i in range(0, len(b), 5):
+        print(b[i: i + 5], end=' ')
 
 
-def atbash_encode(text: str) -> str:
-    pass
-
-
-def atbash_decode(cipher: str) -> str:
-    pass
+print(len_5(atbash_code("Bambarbia, Kirgudu")))
