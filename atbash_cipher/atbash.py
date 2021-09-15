5320e6edf2a4a65e234f204a0c9c9a8945d9ee49
alf191 <88853107+alf191@users.noreply.github.com>
2021-09-15 23:50:54 +0300

    Update atbash.py

--- atbash_cipher/atbash.py
+++ atbash_cipher/atbash.py
@@ -22,3 +22,17 @@ def atbash_encode(text: str) -> str:
 
 def atbash_decode(cipher: str) -> str:
     pass
+
+import string
+
+def atbash_code(j: str):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    y = j.lower().replace(' ','').translate(str.maketrans(' ', ' ', string.punctuation + '—'))
+
+    return y.translate(str.maketrans(alphabet + alphabet.upper(), alphabet[::-1] + alphabet.upper()[::-1]))
+def len_5(b: str):
+
+    for i in range(0, len(b), 5):
+        print(b[i: i + 5], end=' ')
+        
+ print(len_5(atbash_code("Bambarbia, Kirgudu")))
