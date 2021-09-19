2ee36fbf2abdd23803ddc9f1e42e8e2cbb6c2138
alf191 <88853107+alf191@users.noreply.github.com>
2021-09-19 23:39:17 +0300

    Update and rename numeric_palindrom.py to Dz_V_num_palindrom.py

--- numeric_palindrom/Dz_V_num_palindrom.py
+++ numeric_palindrom/Dz_V_num_palindrom.py
@@ -12,3 +12,22 @@ def is_palindrom(n: int) -> bool:
         raise ValueError("Number must be a positive integer")
 
     pass
+
+
+
+
+def rev(b):
+    num = 0
+    while b > 0:
+        num = num * 10 + b % 10
+        b = b // 10
+    return num
+
+
+def num_pal(b: int) -> bool:
+    if b == rev(b):
+        return True
+    else:
+        return False
+
+print(num_pal(50505))
